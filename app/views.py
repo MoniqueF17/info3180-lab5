ba5fc487f262c644ea562ad55ceca3d2f1b68be0
moniquef17 <moniquefrancis@live.com>
2019-03-23 19:49:46 +0000

    logout route created (step 10)

--- app/views.py
+++ app/views.py
@@ -71,6 +71,14 @@ def login():
 @login_manager.user_loader
 def load_user(id):
     return UserProfile.query.get(int(id))
+    
+@app.route("/logout")
+@login_required
+def logout():
+    # Logout the user and end the session
+    logout_user()
+    flash('You have been logged out.', 'danger')
+    return redirect(url_for('home'))
 
 ###
 # The functions below should be applicable to all Flask apps.
